ea636ae18c952523572585b03d2345ab06ecf9f0
geonnu <geonnu777@naver.com>
2025-06-10 03:38:35 +0900

    Update main.py

--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -9,11 +9,12 @@ def main():
         print("1. 지출 추가")
         print("2. 지출 목록 보기")
         print("3. 총 지출 보기")
-        print("4. 종료")
-        choice = input("선택 > ")
-
-        if choice == "1":
-            category = input("카테고리 (예: 식비, 교통 등): ")
+        print("4. 후회 지출 보기")
+        print("5. 종료")
+        choice = input("선택: ")
+    
+        if choice == '1':
+            category = input("카테고리 (예: 식비, 교통 등):  ")
             description = input("설명: ")
             try:
                 amount = int(input("금액(원): "))
@@ -21,19 +22,17 @@ def main():
                 print("잘못된 금액입니다.\n")
                 continue
             budget.add_expense(category, description, amount)
-
-        elif choice == "2":
+        elif choice == '2':
             budget.list_expenses()
-
-        elif choice == "3":
+        elif choice == '3':
             budget.total_spent()
-
-        elif choice == "4":
-            print("가계부를 종료합니다.")
+        elif choice == '4':
+            budget.regret_spending()
+        elif choice == '5':
+            print("종료합니다.")
             break
-
         else:
-            print("잘못된 선택입니다.\n")
+            print("잘못된 입력입니다.\n")
 
 
 if __name__ == "__main__":
