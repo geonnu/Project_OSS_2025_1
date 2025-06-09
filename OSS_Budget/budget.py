39f500298a613dc40c2601518aaaca40a0a3eb5c
geonnu <geonnu777@naver.com>
2025-06-10 03:54:49 +0900

    Update budget.py

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -7,6 +7,15 @@ class Budget:
 
     def add_expense(self, category, description, amount):
         today = datetime.date.today().isoformat()
+        while True:
+            try:
+                satisfaction = int(input("이 지출에 대한 만족도를 1~5점으로 입력하세요: "))
+                if 1 <= satisfaction <= 5:
+                    break
+                else:
+                    print("1부터 5 사이의 숫자를 입력하세요.")
+            except ValueError:
+                print("정수로 입력해주세요.")
         expense = Expense(today, category, description, amount)
         self.expenses.append(expense)
         print("지출이 추가되었습니다.\n")
@@ -23,5 +32,14 @@ class Budget:
     def total_spent(self):
         total = sum(e.amount for e in self.expenses)
         print(f"총 지출: {total}원\n")
-
+        
+    def regret_spending(self):
+        low_scores = [e for e in self.expenses if e.satisfaction <= 2]
+        if not low_scores:
+            print("후회되는 지출이 없습니다.\n")
+            return
+        print("\n[후회 지출 목록]")
+        for e in low_scores:
+            print(f"- {e}")
+        print()
 
