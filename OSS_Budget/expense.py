6ddc8f2d5c7e9d39228abaed215ac4979cc6b37b
geonnu <geonnu777@naver.com>
2025-06-10 03:55:28 +0900

    Update expense.py

--- OSS_Budget/expense.py
+++ OSS_Budget/expense.py
@@ -1,10 +1,12 @@
 
 class Expense:
-    def __init__(self, date, category, description, amount):
+    def __init__(self, date, category, description, amount, satisfaction):
         self.date = date
         self.category = category
         self.description = description
         self.amount = amount
+        self.satisfaction = satisfaction  # 1~5점
 
     def __str__(self):
-        return f"[{self.date}] {self.category} - {self.description}: {self.amount}원"
+        return (f"{self.date} | {self.category} | {self.description} | "
+                f"{self.amount}원 | 만족도: {self.satisfaction}/5")
